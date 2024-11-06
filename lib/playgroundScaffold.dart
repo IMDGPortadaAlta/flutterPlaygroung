5e477b6ac38db42cd497b3a85954b20fa393eb2e
Ignacio Dollenart <al.ignacio.dollenart.garcia@iesportada.org>
2024-11-06 23:10:30 +0100

    Added playgroundScaffold for make the widgets tree in an individual dart doc

---
+++ lib/playgroundScaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlaygroundScaffold extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _stateScaffold();
+}
+
+class _stateScaffold extends State<PlaygroundScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO make some trys and build from here
+    );
+  }
+}
