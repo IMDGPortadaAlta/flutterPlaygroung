2d887703b71cf98c8939bbc4d9c92eb7da837d50
Ignacio <al.ignacio.dollenart.garcia@iesportada.org>
2024-11-07 12:16:17 +0100

    added TapBar and NavBar

--- lib/playgroundScaffold.dart
+++ lib/playgroundScaffold.dart
@@ -10,8 +10,28 @@ class PlaygroundScaffold extends StatefulWidget {
 class _stateScaffold extends State<PlaygroundScaffold> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return  DefaultTabController(length: 2, 
+    child: Scaffold(
+      appBar: AppBar(
+        title: const Text("mainAppBar"),
+        actions: [
+          Builder(builder: (context) {
+            return IconButton(onPressed: (){
+              TabController controller = DefaultTabController.of(context);
+              controller.animateTo(1);
+            }, icon: Icon(Icons.info));
+          })
+        ],
+      ),
+      body: const TabBarView(children: <Widget> [
+        Expanded(child: Text("Contact Data List"),),
+        Expanded(child: Text("Favorite List"))
+      ]),
+      bottomNavigationBar: const TabBar(tabs: <Widget>[
+          Tab(icon: Icon(Icons.contacts_sharp), text: "Contactos"),
+          Tab(icon: Icon(Icons.star), text: "Favoritos"),
+        ],),
+    )
     );
   }
 }
